957d86ea21e80e8f77c91f26c46e9e696dd7bce3
Eddie <eddiepincay@gmail.com>
2018-12-19 23:10:40 -0500

    Adding Changes

--- SacrMeetPlanner/SacrMeetPlanner/Pages/Meetings/Index.cshtml.cs
+++ SacrMeetPlanner/SacrMeetPlanner/Pages/Meetings/Index.cshtml.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using SacrMeetPlanner.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace SacrMeetPlanner.Pages.Meetings
 {
@@ -20,8 +21,34 @@ namespace SacrMeetPlanner.Pages.Meetings
 
         public IList<Meeting> Meeting { get;set; }
 
+        //Eddie Pincay
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        // Requires using Microsoft.AspNetCore.Mvc.Rendering;
+        public SelectList Presidings { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string MeetingPresiding { get; set; }
+
         public async Task OnGetAsync()
         {
+            // Use LINQ to get list of Presiding.
+            IQueryable<string> presidQuery = from m in _context.Meeting
+                                             orderby m.Presiding
+                                             select m.Presiding;
+
+            var meetings = from m in _context.Meeting
+                           select m;
+            if (!String.IsNullOrEmpty(SearchString))
+            {
+                meetings = meetings.Where(s => s.Conducting.Contains(SearchString));
+            }
+
+            if (!String.IsNullOrEmpty(MeetingPresiding))
+            {
+                meetings = meetings.Where(x => x.Presiding == MeetingPresiding);
+            }
+            Presidings = new SelectList(await presidQuery.Distinct().ToListAsync());
+
             Meeting = await _context.Meeting.ToListAsync();
         }
     }
